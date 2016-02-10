8f512a7b8bbd7479f0fabead8527a36bc0c1cab9
Serdar Karakaş <serdarkarakas@Serdar-Karakas.local>
2016-02-10 14:37:56 +0200

    first Commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+       mailButton.setTitle("Gönderildi", forState: UIControlState.Normal)
+       // mailButton.hidden=true;
+        
+        /*if mySwitch.on{
+        messageLabel.text = "yes"
+            mySwitch.setOn(false, animated: false)
+        }else{
+        messageLabel.text = "no"
+                        mySwitch.setOn(true, animated: true)
+        }
+        */
+        
+        messageLabel.textColor = UIColor.redColor();
+        enterMessageTextField.resignFirstResponder()
+        
+  }
+    
+    
+    
+    
+    //1.35:26 ilk video
 
 }
 
